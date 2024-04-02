b6834830f8c629ec88385a7aad64d11935202554
nick-fallon <110064775+nick-fallon@users.noreply.github.com>
2024-04-02 14:17:18 -0700

    Add prettier (#6)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,46 @@
-'use client'
+'use client';
 
-import {createClient} from "@/utils/supabase/client";
-import {useEffect, useState} from "react";
+import { createClient } from '@/utils/supabase/client';
+import { useEffect, useState } from 'react';
 
 const supabase = createClient();
 
 const getProfiles = async () => {
   const { data, error } = await supabase.from('profiles').select();
   return data;
-}
+};
 
 export default function Home() {
-    const [profiles, setProfiles] = useState<null | any[]>(null);
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(false);
+  const [profiles, setProfiles] = useState<null | any[]>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
 
-    useEffect(() => {
-        (async () => {
-            try {
-                const data = await getProfiles();
-                setProfiles(data);
-                setLoading(false);
-            } catch (error) {
-                setError(true);
-                setLoading(false);
-            }
-        })()
-    }, [])
+  useEffect(() => {
+    (async () => {
+      try {
+        const data = await getProfiles();
+        setProfiles(data);
+        setLoading(false);
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    })();
+  }, []);
 
-    if (loading) {
-        return (
-            <pre>
-                Loading...
-            </pre>
-        )
-    }
+  if (loading) {
+    return <pre>Loading...</pre>;
+  }
 
-    if (error) {
-        return (
-            <pre>Error</pre>
-        )
-    }
+  if (error) {
+    return <pre>Error</pre>;
+  }
   return (
-      <main>
-          {profiles && profiles?.map((profile) => (
-              <div key={profile.id}>
-                  {profile.full_name}
-              </div>
-          ))}
-      </main>
+    <main>
+      {profiles &&
+        profiles?.map((profile) => (
+          <div key={profile.id}>{profile.full_name}</div>
+        ))}
+    </main>
   );
 }
